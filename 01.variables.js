f113ccfbb34d7fcd09333ca23f7280d1c1f46695
Anirban19-99 <anirban2032@gmail.com>
2023-09-18 21:16:04 +0530

    variables

---
+++ 01.variables.js
@@ -0,0 +1,8 @@
+/*we can use only const and let while declaring variable;
+var can't be use because it do not have block scope; */
+const accountNumber=12162010
+let name= "something"
+let state
+var do_not_use="not used anymore" // no
+
+console.table([accountNumber,name,state,do_not_use])
